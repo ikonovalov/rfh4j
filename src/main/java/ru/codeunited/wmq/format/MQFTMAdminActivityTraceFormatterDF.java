40e8655339b3f8c87b84e59ca2cb3fd16a7865d1
ikonovalov <konovalov84@gmail.com>
2015-03-30 19:22:11 +0300

    Remove old code

--- src/main/java/ru/codeunited/wmq/format/MQFTMAdminActivityTraceFormatterDF.java
+++ src/main/java/ru/codeunited/wmq/format/MQFTMAdminActivityTraceFormatterDF.java
@@ -96,65 +96,6 @@ public class MQFTMAdminActivityTraceFormatterDF extends MQPCFMessageAbstractForm
             allowOutput = true;
         }
 
-        // scan for MQGACF_ACTIVITY_TRACE
-        /*Enumeration<PCFParameter> parametersL1 = pcfMessage.getParameters();
-        while (parametersL1.hasMoreElements()) {
-            final PCFParameter parameter = parametersL1.nextElement();
-
-
-            // process activity trace elements (MQGACF_ACTIVITY_TRACE is always grouped as MQCFGR)
-            MQCFGR trace = (MQCFGR) parameter; // => MQGACF_ACTIVITY_TRACE
-
-            // timestamp;msgId;queueName;operation(put/get);QMGRNAme;size;<Строка заголовков - вида name=value>;
-            boolean xmitExchange = "MQXMIT".equals(decodedParameter(trace, MQCACH_FORMAT_NAME));
-            final String putDateTime = String.format("%s %s", decodedParameter(trace, MQCACF_PUT_DATE), decodedParameter(trace, MQCACF_PUT_TIME));
-            try {
-                buffer.append(TIME_REFORMATED.format(TIME_FORMAT.parse(putDateTime)));
-            } catch (ParseException e) {
-                buffer.append(putDateTime);
-            } finally {
-                buffer.append(';');
-            }
-            buffer.append( *//* append message id *//*
-                    xmitExchange ?
-                            decodedParameter(trace, MQBACF_XQH_MSG_ID) :
-                            decodedParameter(trace, MQBACF_MSG_ID))
-                    .append(';');
-
-            buffer.append( *//* append queue name *//*
-                    xmitExchange ?
-                            String.format("%s;%s;",
-                                    coalesce(trace, MQCACF_OBJECT_NAME, MQCACF_RESOLVED_Q_NAME, MQCACF_RESOLVED_LOCAL_Q_NAME),
-                                    decodedParameter(trace, MQCACF_XQH_REMOTE_Q_NAME)
-                            ) :
-                            String.format("%s;;",
-                                    coalesce(trace, MQCACF_OBJECT_NAME, MQCACF_RESOLVED_LOCAL_Q_NAME, MQCACF_RESOLVED_LOCAL_Q_NAME)
-                            )
-            );
-
-            buffer.append(decodeValue(mqiacfOperation)).append(';');
-
-            buffer.append( *//* append queuemanager name *//*
-                    xmitExchange ?
-                            String.format("%s;%s;",
-                                    coalesce(trace, MQCACF_OBJECT_Q_MGR_NAME, MQCACF_RESOLVED_Q_MGR, MQCACF_RESOLVED_LOCAL_Q_MGR),
-                                    decodedParameter(trace, MQCACF_XQH_REMOTE_Q_MGR)
-                            ) :
-                            String.format("%s;;",
-                                    coalesce(trace, MQCACF_OBJECT_Q_MGR_NAME, MQCACF_RESOLVED_Q_MGR, MQCACF_RESOLVED_LOCAL_Q_MGR)
-                            )
-            );
-
-            buffer.append(decodedParameter(trace, MQIACF_MSG_LENGTH)).append(';');
-            buffer.append(decodedParameter(pcfMessage, MQIA_APPL_TYPE)).append(';');
-            buffer.append(decodedParameter(pcfMessage, MQCACF_APPL_NAME)).append(';');
-            buffer.append(decodedParameter(pcfMessage, MQCACF_USER_IDENTIFIER));
-            //buffer.append(decodedParameter(trace, MQBACF_MESSAGE_DATA)).append(';');
-
-            buffer.append('\n');
-            allowOutput = true;
-
-        }*/
         if (!allowOutput) { // drop buffer in it contains nothing interesting.
             buffer.setLength(0);
         }
