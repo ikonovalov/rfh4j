4dc57276b0c37b538cce417748f940241ee79978
ikonovalov <konovalov84@gmail.com>
2015-04-24 00:01:10 +0300

    Start scala works

---
+++ src/main/scala/ru/codeunited/wmq/MyScala.scala
@@ -0,0 +1,10 @@
+package ru.codeunited.wmq
+
+/**
+ * codeunited.ru
+ * konovalov84@gmail.com
+ * Created by ikonovalov on 23.04.15.
+ */
+case class MyScala(x: Int, y: Int);
+
+case class Event(msg: String)
